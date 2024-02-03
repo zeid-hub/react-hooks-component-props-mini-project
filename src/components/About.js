530428337447c9896a6e88760d747285adab86e9
zeid <zeidhassan4@gmail.com>
2024-02-03 16:42:22 +0300

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,15 @@
-import React from "react"
+import React from 'react';
 
-function About (props){
-
-    const imageUrl = props.image ||"https://via.placeholder.com/215";
-    return(
-      <aside>
-          <img src={{imageUrl}} alt="blog logo"></img>
-          <p> {props.about} </p>          
-      </aside>
-    )
+function About(props) {
+  
+  const imageUrl = props.image || "https://via.placeholder.com/215";
+  
+  return (
+    <aside>
+      <img src={imageUrl} alt="blog logo" />
+      <p>{props.about}</p>
+    </aside>
+  );
 }
 
-export default About
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import blogData from "../data/blog";
-import Header from "./Header"
-import About from "./About"
-import ArticleList from "./ArticleList"
-
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      <Header name={blogData.name}/>
-      <About image={blogData.image} about={blogData.about}/>
-      <ArticleList posts={blogData.post}/>
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
+      
     </div>
   );
 }
